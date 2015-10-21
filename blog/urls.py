4ab2d2c55a608754b4741ea35886fe73076c4587
Vishal Taj <getvishalonline@gmail.com>
2015-10-21 17:38:05 +0530

    Update after basic styling

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url
 from . import views 
 
+
 urlpatterns = [
                url(r'^$', views.post_list, name='post_list'),
-               url(r'^(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+               url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+               url(r'^post/new/$', views.post_new , name='post_new'),
+               url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
                ]
+
 
 
 # ^(?P<question_id>[0-9]+)/vote/
