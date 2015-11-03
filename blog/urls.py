aab84a89cd5fc5d8d98bc851dedaef688a7fefea
Vishal Taj <getvishalonline@gmail.com>
2015-11-03 14:37:11 +0530

    re implement

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
-from . import views 
+from . import views
+from .views import PostListView, PostDetailView
 
 
 urlpatterns = [
-               url(r'^$', views.post_list, name='post_list'),
-               url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
-               url(r'^post/new/$', views.post_new , name='post_new'),
+               url(r'^$', PostListView.as_view()),
+               url(r'^post/(?P<pk>[0-9]+)/$', PostDetailView.as_view(), name='post_detail'),
+               url(r'^new/$', views.post_new , name='post_new'),
                url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
                ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,27 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.core.urlresolvers import reverse
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404, redirect, render_to_response
 from .models import Post
 from django.utils import timezone
 from .forms import PostForm
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html',{'posts':posts})
+# def post_list(request):
+#     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+#     return render(request, 'blog/post_list.html',{'posts':posts})
+#
+# def post_detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     return render(request, 'blog/post_detail.html', {'post': post })
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post })
+class PostListView(ListView):
+    model = Post
+    queryset = Post.objects.all()
+    print queryset
+
+class PostDetailView(DetailView):
+    model = Post
+    queryset = Post.objects.all()
+    print queryset
 
 
 def post_new(request):
@@ -19,11 +31,13 @@ def post_new(request):
             post = form.save(commit=False)
             post.author = request.user
             post.published_date = timezone.now()
-            post.save() 
-            return redirect('blog.views.post_detail', pk=post.pk)
+            post.save()
+            url = reverse('posts:post_detail',args=(),
+                    kwargs={'pk':post.pk})
+            return redirect(url)
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'post_add.html', {'form': form})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -34,7 +48,9 @@ def post_edit(request, pk):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('blog.views.post_detail', pk=post.pk)
+            url = reverse('posts:post_detail',args=(),
+                    kwargs={'pk':post.pk})
+            return redirect(url)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'post_edit.html', {'form': form})
